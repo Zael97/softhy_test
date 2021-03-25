1e68d04abf683ce52c67654506f3ce27a7f2f2c5
Zael97 <amunante@aynitech.com>
2021-03-24 20:40:51 -0500

    estudiante register v1

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,25 @@
 const pool=require('../db');
-exports.estudiantes_post = async(req, res, next)=>{
+exports.estudiante_post = async(req, res, next)=>{
+    const filename = req.file.filename;
+    const { nombre, ap_paterno, ap_materno, grado, edad }= req.body;
+    pool.connect((err, client, done)=>{
+        //const query ='call register('Anthony', 'Muñante', 'Chávez', 'segundo', '1995-11-06', '/uploads/avatar.png');'
+        const query = 'CALL register($1, $2, $3, $4,$5, $6) returning *';
+        const values =[nombre, ap_paterno, ap_materno, grado, edad, filename];
+        client.query(query, values, (error, result)=>{
+            done();
+            if(error){
+                res.status(400).json({error: error.message});
+            }
+            res.status(202).send({
+                status: 'SUccessful',
+                result: result.rows[0],
+            });
+        })
+    })
 }
 
-exports.estudiantes_delete = async(req, res, next)=>{
+exports.estudiante_delete = async(req, res, next)=>{
 
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 const controllers = require('../controllers/controllers');
 const { Router } = require('express');
 const router = Router();
+const multer = require('multer');
+const upload = multer({dest:'../uploads/'});
 
 
-
-router.post('/estudiantes',controllers.estudiantes_post);
-router.delete('/estudiantes', controllers.estudiantes_delete);
+router.post('/estudiante',upload.single('avatar'),controllers.estudiante_post);
+router.delete('/estudiante', controllers.estudiante_delete);
 router.get('/grados',controllers.grados_get);
 router.get('/reporte', controllers.reportes_get);
 
